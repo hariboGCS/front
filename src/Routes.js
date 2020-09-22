b3658849929b228530fa46c955dc9b9bb4fc35cb
towel1017 <towel1017@naver.com>
2020-09-22 23:48:57 +0900

    [change]

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import { DashBoard, AddScore, Ranking, SearchInfo, Setting } from './components/Main'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import {
+    DashBoard,
+    AddScore,
+    Ranking,
+    SearchInfo,
+    Setting,
+} from './components/Main'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/DashBoard" exact component={DashBoard}/>
-                <Route path="/AddScore" component={AddScore}/>
-                <Route path="/Ranking" component={Ranking}/>
-                <Route path="/SearchInfo" component={SearchInfo}/>
-                <Route path="/Setting" component={Setting}/>
+                <Route path="/DashBoard" exact component={DashBoard} />
+                <Route path="/AddScore" component={AddScore} />
+                <Route path="/Ranking" component={Ranking} />
+                <Route path="/SearchInfo" component={SearchInfo} />
+                <Route path="/Setting" component={Setting} />
             </Switch>
         </BrowserRouter>
     )
